0cdca38702056a176217460fc1e5f5c77263eace
github username <github email address>
2020-02-15 16:28:36 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
-const shortGreeting = `Welcome, ${currentUser[0]}!`
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
